d7444e3493463370c32ddabad891c9d8a4f46ceb
Anderson Correia <acorreia610@gmail.com>
2022-05-14 11:30:42 -0300

    Project created

---
+++ backend/src/main/java/com/ac/churrascalc/ChurrascalcApplication.java
@@ -0,0 +1,13 @@
+package com.ac.churrascalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurrascalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurrascalcApplication.class, args);
+	}
+
+}
