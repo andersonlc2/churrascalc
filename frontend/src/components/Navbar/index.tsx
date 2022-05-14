6e581b8965e90c21b11af43610410d6d0ee4403a
Anderson Correia <acorreia610@gmail.com>
2022-05-14 13:49:12 -0300

    Header

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>Churras Calculadora</h1>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
